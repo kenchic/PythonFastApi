8239efadf0dded1abf13ce4aae629759f979fcec
Germán Alvarez <kecnhic@gmail.com>
2025-06-18 04:46:14 +0000

    adicion de 2 endpionts

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from fastapi import fastapi
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get('/', tags=['Inicio'])
+def home():
+    return {"mensaje": "hola mundo."}
+
+@app.get('/usuarios', tags=['Inicio'])
+def usuario():
+    return "muchos usuarios"
